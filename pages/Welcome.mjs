c7bb831063dccad70e398f9099514ab737879719
GustafJ <gustaf89@hotmail.se>
2022-03-04 13:45:16 +0100

    fix bug

--- pages/Welcome.mjs
+++ pages/Welcome.mjs
@@ -4,9 +4,9 @@ export default class Welcome {
   static show() {
     console.clear();
     console.log(
-      'Hello and welcome to this odd experience!',
-      '\nThis webpage is, in its entirety, based in the console.',
-      '\nType %cGoTo.mainMenu() %cand press enter to get to the menu.',
+      `Hello and welcome to this odd experience!
+      \nThis webpage is, in its entirety, based in the console.
+      \nType %cGoTo.mainMenu() %cand press enter to get to the menu.`,
       'color: lime;',
       'color: inherit;'
     );
