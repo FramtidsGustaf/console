85f333c4e54466f16dfbef4d253d4ee89529564d
GustafJ <gustaf89@hotmail.se>
2022-03-04 13:41:10 +0100

    add style to welcome

--- pages/Welcome.mjs
+++ pages/Welcome.mjs
@@ -1,14 +1,15 @@
-"use strict"
+'use strict';
 
 export default class Welcome {
-  
   static show() {
     console.clear();
     console.log(
       'Hello and welcome to this odd experience!',
       '\nThis webpage is, in its entirety, based in the console.',
-      '\nType GoTo.mainMenu() and press enter to get to the menu.'
-      );
+      '\nType %cGoTo.mainMenu() %cand press enter to get to the menu.',
+      'color: lime;',
+      'color: inherit;'
+    );
   }
 }
 
