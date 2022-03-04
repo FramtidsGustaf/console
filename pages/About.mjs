8e1791fb8a0848a8820c302c50cff4576f8521bf
GustafJ <gustaf89@hotmail.se>
2022-03-04 13:28:49 +0100

    minor styling

--- pages/About.mjs
+++ pages/About.mjs
@@ -3,6 +3,6 @@
 export default class About {
   static show() {
     console.clear();
-    console.log('An aspiring web developer. Vue.js seems to be my cup of tea. Please checkout my contact page. (type GoTo.contact() and press enter)');
+    console.log('An aspiring web developer. Vue.js seems to be my cup of tea. Please checkout my contact page. (type %cGoTo.contact() and press enter)', 'color: lime');
   }
 }
